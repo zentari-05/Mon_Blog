cbb9980a74418e7ddaf7c6bc27eb04d04bfad367
zentari <zentariabderahman05@gmail.com>
2025-11-21 13:18:38 +0100

    index.php vueAccueil.php Modele.php

--- vueAccueil.php
+++ vueAccueil.php
@@ -1,14 +1,32 @@
-<?php $titre = 'Mon Blog'; ?>
-<?php ob_start(); ?>
-<?php foreach ($billets as $billet): ?>
-<article>
-<header>
-<h1 class="titreBillet"><?= $billet['titre'] ?></h1>
-<time><?= $billet['date'] ?></time>
-</header>
-<p><?= $billet['contenu'] ?></p>
-</article>
-<hr />
-<?php endforeach; ?>
-<?php $contenu = ob_get_clean(); ?>
-<?php require 'gabarit.php'; ?>
+<!doctype html> 
+<html lang="fr"> 
+    <head> 
+        <meta charset="UTF-8" /> 
+        <link rel="stylesheet" href="style.css" /> 
+        <title>Mon Blog</title> 
+    </head> 
+    <body> 
+        <div id="global"> 
+            <header> 
+                <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>         
+                <p>Je vous souhaite la bienvenue sur ce modeste blog.</p> 
+            </header> 
+        <div id="contenu"> 
+    <?php foreach ($billets as $billet): ?> 
+    <article> 
+        <header> 
+            <h1 class="titreBillet"><?= $billet['titre'] ?></h1> 
+            <time><?= $billet['date'] ?></time> 
+        </header> 
+    <p><?= $billet['contenu'] ?></p> 
+    </article> 
+    <hr /> 
+    <?php endforeach; ?> 
+    </div> <!-- #contenu --> 
+        <footer id="piedBlog">         
+Blog 
+réalisé avec PHP, HTML5 et CSS. 
+        </footer> 
+    </div> <!-- #global --> 
+    </body> 
+</html> 
